a6ce0e67b7bd094dbf1056e0f6dd9bcaaeceb3e1
D-Melita <dgmelita9@gmail.com>
2022-01-25 21:20:47 +0000

    added small fix

--- parte-2/client/tecnicofs_client_api.c
+++ parte-2/client/tecnicofs_client_api.c
@@ -45,7 +45,7 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_MOUNT;
     
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -54,6 +54,7 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     ack = read(client.rx, buf, BUFFER_SIZE - 1);
     buf[ack] = 0;
 
+    //send_msg_str(client.tx, client_pipe_path)
     ret = write(client.tx, client_pipe_path, sizeof(char) * (strlen(client_pipe_path) + 1));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -78,7 +79,7 @@ int tfs_unmount() {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_UNMOUNT;
     
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -118,7 +119,7 @@ int tfs_open(char const *name, int flags) {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_OPEN;
     
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -163,7 +164,7 @@ int tfs_close(int fhandle) {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_CLOSE;
 
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -199,7 +200,7 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_WRITE;
 
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -235,7 +236,7 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     ack = read(client.rx, buf, BUFFER_SIZE - 1);
     buf[ack] = 0;
     
-    //send_msg_str(client.tx, &buffer);
+    //send_msg_str(client.tx, &buffer); - este nao pq é sizeof(char)*len e nao strlen
     ret = write(client.tx, &buffer, sizeof(char)*len);
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -253,7 +254,7 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_READ;
 
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -298,7 +299,7 @@ int tfs_shutdown_after_all_closed() {
     char buf[BUFFER_SIZE];
     char op_code = TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED;
 
-    //send_msg_str(client.tx, &op_code);
+    //send_msg_opcode(client.tx, &op_code);
     ret = write(client.tx, &op_code, sizeof(char));
     if (ret == -1) {
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
@@ -325,9 +326,15 @@ int tfs_shutdown_after_all_closed() {
 // ALSO FOR READ AND WRITE IT'S NEEDED (SUPPOSED TO RETURN 
 // THE NUMBER OF BYTES WRITTEN/READ)
 
-/*void send_msg_str(int tx, char const* buffer){
+/*void send_msg_opcode(int tx, char const* buffer){
     ssize_t ret;
     ret = write(tx, buffer, sizeof(char));
+    check_errors(ret);
+}
+
+void send_msg_str(int tx, char const* buffer){
+    ssize_t ret;
+    ret = write(tx, buffer, sizeof(char)*strlen(buffer));
     check_errors(ret);
 }
 
